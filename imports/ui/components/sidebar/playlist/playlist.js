f90b02c4a37e55eddd653aab6e65e4d16496ea46
dylan sanders <sandersd27@gmail.com>
2016-05-17 12:22:37 -0700

    upvote logic/ui in playlist

--- imports/ui/components/sidebar/playlist/playlist.js
+++ imports/ui/components/sidebar/playlist/playlist.js
@@ -1,3 +1,5 @@
+
+
 import './playlist.html';
 
 
@@ -5,5 +7,29 @@ import './playlist.html';
 Template.playlist.helpers({
   posts: function(){
     return Posts.find();
+  },
+  isUpvoted: function() {
+    if(_.contains(this.upvotedBy, Meteor.userId()))
+      return 'upvote-block--active';
+    else
+      return '';
+  },
+});
+
+
+Template.playlist.events({
+  "click .upvote-block": function(event, template){
+    if(Meteor.userId()) {
+      var upvotedPost = this;
+      Meteor.call('upvotePost', upvotedPost._id, function(err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Upvoted!" + upvotedPost._id);
+        }
+      });
+    } else {
+      alert('Please login to upvote posts!');
+    }
   }
 });
