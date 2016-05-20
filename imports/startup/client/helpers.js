3548f91ce9727fd02efbfa517dac17cd057001d6
dylan sanders <sandersd27@gmail.com>
2016-05-20 06:42:33 -0700

    profile now works

--- imports/startup/client/helpers.js
+++ imports/startup/client/helpers.js
@@ -22,3 +22,77 @@ UI.registerHelper("getUserName", function (userId, length) {
       }
     }
 });
+
+UI.registerHelper("profilePadding", function(){
+  return FlowRouter.current().route.name == "Profile" ? '--profile-page' : '' ;
+});
+
+UI.registerHelper("ownProfile", function(){
+  return (FlowRouter.current().params._id == Meteor.userId() ? true : false );
+});
+
+UI.registerHelper("getUserVillagerNum", function (userId) {
+    let user = Meteor.users.findOne({_id: userId});
+    if (user) {
+      return user.profile.villagerNum;
+    } else {
+      return '';
+    }
+});
+
+UI.registerHelper("getUpvotesNum", function (userId) {
+  let posts = Posts.find({"createdBy": userId}).fetch();
+  let upvotesCollected = 0;
+  posts.forEach(function(post) {
+    upvotesCollected += post.upvotedBy.length;
+  });
+
+  return upvotesCollected;
+});
+
+
+UI.registerHelper("getUserUpvoteRank", function (userId) {
+  let user = Meteor.users.findOne({_id: userId});
+  if (user) {
+    let users = Meteor.users.find({});
+    let sortable = [];
+    users.forEach(function(userX) {
+      let posts = Posts.find({"createdBy": userX._id}).fetch();
+      let upvotesCollected = 0;
+      posts.forEach(function(post) {
+        upvotesCollected += post.upvotes;
+      });
+      sortable.push([userX._id, upvotesCollected]);
+    });
+    sortable.sort(function(a, b) {return b[1] - a[1]});
+    let rankList = _.pluck(sortable, 0);
+    return (rankList.indexOf(userId) + 1);
+  }
+});
+
+UI.registerHelper("getNumberEnd", function (num) {
+  let numString = num.toString();
+  let last = numString[(numString.length - 1)];
+  switch (last) {
+    case '1':
+      return 'st'
+      break;
+    case '2':
+      return 'nd'
+      break;
+    case '3':
+      return 'rd'
+      break;
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '0':
+      return 'th'
+      break;
+    default:
+      return ''
+  }
+});
