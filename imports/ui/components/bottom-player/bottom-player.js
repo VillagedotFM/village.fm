9e9a6f8e7bc30872dcff13c8ef8c4be8f4c00f01
dylan sanders <sandersd27@gmail.com>
2016-06-01 15:05:52 -0700

    broke out events

--- imports/ui/components/bottom-player/bottom-player.js
+++ imports/ui/components/bottom-player/bottom-player.js
@@ -1,21 +1,3 @@
 import './bottom-player.html';
 import './helpers.js';
-
-
-Template.bottom_player.events({
-  "click .upvote-block": function(event, template){
-    //TODO: upvoting a post stops it...
-    if(Meteor.userId()) {
-      let upvotedPost = this;
-      Meteor.call('upvotePost', upvotedPost._id, function(err, data) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(upvotedPost);
-        }
-      });
-    } else {
-      alert('Please login to upvote posts!');
-    }
-  },
-});
+import './events.js';
