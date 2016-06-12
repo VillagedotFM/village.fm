c4485a397169669dbc3aac8f9bdaeddd6b4f51b5
dylan sanders <sandersd27@gmail.com>
2016-06-12 02:45:22 -0700

    added comment and examples for long ass youtube regex

--- imports/api/posts/methods.js
+++ imports/api/posts/methods.js
@@ -31,12 +31,40 @@ Meteor.methods({
     }
   },
   getTypeAndId:function(link){
+    //Matches links that are in a valid youtube format and grabs the video id. Does NOT check to see if video exists (line 95)
     const ytRegex = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
+
+    /******* EXAMPLE LINKS ********
+    // *www.youtube-nocookie.com/embed/up_lNV-yoK4?rel=0
+    // *http://www.youtube.com/user/Scobleizer#p/u/1/1p3vcRhsYGo
+    // *http://www.youtube.com/watch?v=cKZDdG9FTKY&feature=channel
+    // *http://www.youtube.com/watch?v=yZ-K7nCVnBI&playnext_from=TL&videos=osPknwzXEas&feature=sub
+    // *http://www.youtube.com/ytscreeningroom?v=NRHVzbJVx8I
+    // *http://www.youtube.com/user/SilkRoadTheatre#p/a/u/2/6dwqZw0j_jY
+    // *http://youtu.be/6dwqZw0j_jY
+    // *http://www.youtube.com/watch?v=6dwqZw0j_jY&feature=youtu.be
+    // *http://youtu.be/afa-5HQHiAs
+    // *http://www.youtube.com/user/Scobleizer#p/u/1/1p3vcRhsYGo?rel=0
+    // *http://www.youtube.com/watch?v=cKZDdG9FTKY&feature=channel
+    // *http://www.youtube.com/watch?v=yZ-K7nCVnBI&playnext_from=TL&videos=osPknwzXEas&feature=sub
+    // *http://www.youtube.com/ytscreeningroom?v=NRHVzbJVx8I
+    // *http://www.youtube.com/embed/nas1rJpm7wY?rel=0
+    // *http://www.youtube.com/watch?v=peFZbP64dsU
+    // *http://youtube.com/v/dQw4w9WgXcQ?feature=youtube_gdata_player
+    // *http://youtube.com/vi/dQw4w9WgXcQ?feature=youtube_gdata_player
+    // *http://youtube.com/?v=dQw4w9WgXcQ&feature=youtube_gdata_player
+    // *http://www.youtube.com/watch?v=dQw4w9WgXcQ&feature=youtube_gdata_player
+    // *http://youtube.com/?vi=dQw4w9WgXcQ&feature=youtube_gdata_player
+    // *http://youtube.com/watch?v=dQw4w9WgXcQ&feature=youtube_gdata_player
+    // *http://youtube.com/watch?vi=dQw4w9WgXcQ&feature=youtube_gdata_player
+    // *http://youtu.be/dQw4w9WgXcQ?feature=youtube_gdata_player
+    *************************/
+
     let ytMatch = link.match(ytRegex);
 
     if (ytMatch) {  //The user-provide url is a link to a youtube video
       let type = 'youtube';
-      let vidId = ytMatch[1]; //[1] is the actual video id
+      let vidId = ytMatch[1]; //[1] is the actual video id from regex ^
       if (vidId.length == 11) { //Length of ytId
         return {
           type: type,
