a9ef36f2c0069f580aa7869e3475b929cdc600c2
dylan sanders <sandersd27@gmail.com>
2016-06-12 05:48:12 -0700

    playlist is now scroll position aware

--- imports/ui/components/sidebar/playlist/playlist.js
+++ imports/ui/components/sidebar/playlist/playlist.js
@@ -1,3 +1,23 @@
 import './playlist.html';
 import './events.js';
 import './helpers.js';
+
+
+Template.playlist.onRendered(function playlistOnRendered(){
+  this.autorun(function(){
+    //On scroll of window
+    $('.wrapper').on('scroll', function(){
+      let scrollTop = $('.wrapper').scrollTop();
+
+      //Find the post (in view) that matches in the playlist
+      let index = parseInt(scrollTop / 850);  //850 is semi arbitrary
+
+      //Remove arrow on everything else and add it to correct playlist item
+      $('.sr-playlist__item').removeClass('sr-playlist__item--active');
+      $('#playlist-' + index).addClass('sr-playlist__item--active');
+
+      //Scroll playlist to the current post
+      $('.sr-playlist').scrollTop(index * 50);
+    });
+  });
+});
