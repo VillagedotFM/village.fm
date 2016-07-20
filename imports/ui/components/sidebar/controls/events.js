069d43f8f9da4bb5f3f8ad445f43c9587e19aa50
dylan sanders <sandersd27@gmail.com>
2016-07-20 03:56:42 -0700

    hooked up latency haxx to prev/next buttons

--- imports/ui/components/sidebar/controls/events.js
+++ imports/ui/components/sidebar/controls/events.js
@@ -8,6 +8,11 @@ Template.controls.events({
       currentPost = appBodyRef.displayPosts.get()[0];
     }
 
+    pauseEverythingElse(currentPost._id);
+    appBodyRef.nowPlaying.set(currentPost);
+
+    $('.post__video-play#'+currentPost._id).hide();
+
     if (currentPost.type === 'youtube') {
       window['ytplayer-' + currentPost._id].playVideo();
     } else {
@@ -37,12 +42,17 @@ Template.controls.events({
     } else {
       //go back a post if there is a prevPost
       if (prevPost) {
+
+        pauseEverythingElse(prevPost._id);
+        appBodyRef.nowPlaying.set(prevPost);
+
+        $('.post__video-play#'+prevPost._id).hide();
+
         if (prevPost.type === 'youtube') {
           window['ytplayer-' + prevPost._id].playVideo();
         } else {
           window['scplayer-' + prevPost._id].play();
         }
-        appBodyRef.nowPlaying.set(prevPost);
       }
     }
   },
@@ -51,12 +61,15 @@ Template.controls.events({
     let nextPost = appBodyRef.nextPost.get();
 
     if (nextPost) {
+      pauseEverythingElse(nextPost._id);
+      appBodyRef.nowPlaying.set(nextPost);
+
+      $('.post__video-play#'+nextPost._id).hide();
       if (nextPost.type === 'youtube') {
         window['ytplayer-' + nextPost._id].playVideo();
       } else {
         window['scplayer-' + nextPost._id].play();
       }
-      appBodyRef.nowPlaying.set(nextPost);
     }
   }
 });
