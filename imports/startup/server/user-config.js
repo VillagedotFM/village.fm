0a430a2004f2c365111702b1ca0c1d55781282a9
dylan sanders <sandersd27@gmail.com>
2016-08-09 21:34:37 -0700

    new users are now added to main village on sign up

--- imports/startup/server/user-config.js
+++ imports/startup/server/user-config.js
@@ -1,7 +1,18 @@
+import { Villages } from '../../api/villages/villages.js';
+
 Accounts.onCreateUser(function(options, user) {
     if (options.profile) {  //Add facebook profile picture
         options.profile.picture = "https://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
         user.profile = options.profile;
     }
+
+    //Add user to main village
+    let villageId = Villages.findOne({})._id;
+    Villages.update({_id:villageId}, {
+      $addToSet: {
+          users: user._id
+      }
+    });
+
     return user;
 });
