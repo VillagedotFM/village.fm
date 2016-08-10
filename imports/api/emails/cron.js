d7026c2ab92119d5b22642304b792e9750667e5d
Matej Hlavacka <matej.hlavacka16@gmail.com>
2016-08-10 16:43:06 +0200

    adjusted email notifications, fixed spiderable

--- imports/api/emails/cron.js
+++ imports/api/emails/cron.js
@@ -12,7 +12,7 @@ console.log('Setting job to future in 10 seconds ' + t);
 
 SyncedCron.add({
   name: 'aggregateNotifications',
-  timezone: 'Australia/Sydney',
+  timezone: 'Asia/Tokyo',
   schedule: function(parser) {
     // Run once a day 
     return parser.text('every 24 hours');
@@ -30,7 +30,7 @@ SyncedCron.add({
             "$gte": lastDay
           },
           "upvotes": {
-            "$gte": 0
+            "$gte": 2
           },
         }
       },
@@ -64,120 +64,132 @@ SyncedCron.add({
 
     _(usersPostingInLastDayAndMostUpvotedPost).each(function(item) {
       // Insert most upvoted post for each user to Emails if larger than 3
-      if(item.post.upvotes >= 3) {
-      	Emails.insert({
-	        to: item._id,
-	        value: 3,
-	        meta: {
-	          count: item.post.upvotes,
-	          postId: item.post.id,
-	        }
-	      });
-      }
+    	Emails.insert({
+        to: item._id,
+        value: 3,
+        meta: {
+          count: item.post.upvotes,
+          postId: item.post.id,
+        }
+      });
+    });
       
-      // Aggregate all Emails for user and select most important one
-      email = Emails.aggregate([{
-        "$match": {
-          "createdAt": {
-            "$gte": lastDay
+    // Aggregate all Emails for user and select most important one
+    emails = Emails.aggregate([{
+      "$match": {
+        "createdAt": {
+          "$gte": lastDay
           },
-          "to": item._id,
+        }
+      },
+      // Aggregate all emails posted within last 24 hours
+      {
+        "$group": {
+          "_id": {
+            user: "$to",
+            meta: "$meta"
+          },
+          "count": {
+            $first: "$value"
+          },
         }
       }, {
         "$sort": {
-          "value": 1
+          "count": 1
         }
       }, {
-        "$limit": 1
-      }]);
-
-      // Switch array of single item to object
-      const emailItem = email[0];
-
-      // Get user details
-      const userDetails = Meteor.users.findOne(item._id).services.facebook;
-
-      // Define let statements
-      let template, sub, post, username;
-
-      // Pick which kind of daily email to send
-      switch (emailItem.value) {
-        case 5:
-          username = Meteor.users.findOne(emailItem.meta.from).profile.name;
-          post = Posts.findOne(emailItem.meta.postId);
-
-          template = "81cb32ef-be4a-4f2d-8f59-995a8b7711b6";
-          sub = {
-            "-firstname-": [userDetails.first_name],
-            "-username-": [username],
-            "-artist-": [post.artist],
-            "-trackname-": [post.title],
-          };
-          break;
-        case 4:
-          username = Meteor.users.findOne(emailItem.meta.from).profile.name;
-          post = Posts.findOne(emailItem.meta.postId);
-
-          // Aggregate Number of Comments for particular post
-          commentsCount = Comments.aggregate([{
-            "$match": {
-              "postId": post._id,
-            }
-          }, {
-            "$group": {
-              "_id": "$postId",
-              "count": {
-                $sum: 1
-              },
-            }
-          }, ]);
-
-          template = "c356a9dd-d16d-4f2e-850f-9d17b17ee449";
-          sub = {
-            "-firstname-": [userDetails.first_name],
-            "-username-": [username],
-            "-comments_count-": [commentsCount[0].count],
-            "-artist-": [post.artist],
-            "-trackname-": [post.title],
-          };
-          break;
-        case 3:
-          post = Posts.findOne(emailItem.meta.postId);
-
-          template = "d0fa08b2-9400-405f-a47e-d09bbda979b1";
-          sub = {
-            "-firstname-": [userDetails.first_name],
-            "-upvotes_count-": [emailItem.meta.count],
-            "-artist-": [post.artist],
-            "-trackname-": [post.title],
-          };
-          break;
-        case 2:
-          // Need to build a function to get emails
-          template = "986445bf-35b4-4fd3-a149-3f5d9a1113ba";
-          break;
-        case 1:
-          // Need to build a function to get emails
-          template = "ca4fdc68-8239-4957-846d-b612c4cb62de";
-          break;
-        default:
-          break;
-      }
-
-      var header = new smtpapi();
-
-      header.setFilters({
-        "templates": {
-          "settings": {
-            "enable": 1,
-            "template_id": template
-          }
+        "$group": {
+          "_id": "$_id.user",
+          "meta": { $first: "$_id.meta"},
+          "value": { $first: "$count"}
         }
-      });
+    }]);
 
-      header.setSubstitutions(sub)
+    _(emails).each(function(email) {
+    	// Get user details
+    	const userDetails = Meteor.users.findOne(email._id).services.facebook;
 
-      // Send usin Nodemailer
+    	// Define let statements
+    	let template, sub, post, username;
+
+    	// Pick which kind of daily email to send
+	    switch (email.value) {
+	      case 5:
+	        username = Meteor.users.findOne(email.meta.from).profile.name;
+	        post = Posts.findOne(email.meta.postId);
+
+	        template = "81cb32ef-be4a-4f2d-8f59-995a8b7711b6";
+	        sub = {
+	          "-firstname-": [userDetails.first_name],
+	          "-username-": [username],
+	          "-artist-": [post.artist],
+	          "-trackname-": [post.title],
+	        };
+	        break;
+	      case 4:
+	        username = Meteor.users.findOne(email.meta.from).profile.name;
+	        post = Posts.findOne(email.meta.postId);
+
+	        // Aggregate Number of Comments for particular post
+	        commentsCount = Comments.aggregate([{
+	          "$match": {
+	            "postId": post._id,
+	          }
+	        }, {
+	          "$group": {
+	            "_id": "$postId",
+	            "count": {
+	              $sum: 1
+	            },
+	          }
+	        }, ]);
+
+	        template = "c356a9dd-d16d-4f2e-850f-9d17b17ee449";
+	        sub = {
+	          "-firstname-": [userDetails.first_name],
+	          "-username-": [username],
+	          "-comments_count-": [commentsCount[0].count],
+	          "-artist-": [post.artist],
+	          "-trackname-": [post.title],
+	        };
+	        break;
+	      case 3:
+	        post = Posts.findOne(email.meta.postId);
+
+	        template = "d0fa08b2-9400-405f-a47e-d09bbda979b1";
+	        sub = {
+	          "-firstname-": [userDetails.first_name],
+	          "-upvotes_count-": [email.meta.count],
+	          "-artist-": [post.artist],
+	          "-trackname-": [post.title],
+	        };
+	        break;
+	      case 2:
+	        // Need to build a function to get emails
+	        template = "986445bf-35b4-4fd3-a149-3f5d9a1113ba";
+	        break;
+	      case 1:
+	        // Need to build a function to get emails
+	        template = "ca4fdc68-8239-4957-846d-b612c4cb62de";
+	        break;
+	      default:
+	        break;
+	    }
+
+	    var header = new smtpapi();
+
+	    header.setFilters({
+	      "templates": {
+	        "settings": {
+	          "enable": 1,
+	          "template_id": template
+	        }
+	      }
+	    });
+
+	    header.setSubstitutions(sub);
+
+	    // Send usin Nodemailer
 			var headers = { "x-smtpapi": header.jsonString() };
 
 			var smtpTransport = nodemailer.createTransport(GlobalServer.emailSettings);
