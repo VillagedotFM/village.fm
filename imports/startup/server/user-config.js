5861e0ec54123c7f39769cf1abb8b4880bd22817
Cristian Ambrozie <cristi@icenodes.com>
2016-08-13 22:00:01 +0300

    Revert "Fixed error when creating fake users: added check to only send emails to facebook users"
    This reverts commit 4a65f6b16fd42c0afdeba365156a97a221272ec8.

--- imports/startup/server/user-config.js
+++ imports/startup/server/user-config.js
@@ -34,47 +34,39 @@ Accounts.onCreateUser(function (options, user) {
       }
     });
 
-    //send email to facebook user (fake users are normal users with password and don't require emails being sent)
-    /**
-     * TODO: perhaps move this into a method outside this one and call it here,
-     * TODO: it will also remove the double check for facebook:  if (user.services.facebook) {..}
-     */
+    var header = new smtpapi();
 
-    if (user.services.facebook) {
-        var header = new smtpapi();
+    var sub = { "-firstname-": [user.services.facebook.first_name] };
 
-        var sub = { "-firstname-": [user.services.facebook.first_name] };
-
-        header.setFilters({
-            "templates": {
-                "settings": {
-                    "enable": 1,
-                    "template_id": "ce551d09-ce7d-4a48-b5b9-653ad2789702"
-                }
-            }
-        });
-
-        header.setSubstitutions(sub);
-
-        // Send usin Nodemailer
-        var headers = { "x-smtpapi": header.jsonString() };
-
-        var smtpTransport = nodemailer.createTransport(GlobalServer.emailSettings);
-
-        var mailOptions = {
-            from:     "Village.fm <hello@village.fm>",
-            to:       user.services.facebook.email,
-            text:     "Hello world",
-            html:     "<b>Hello world</b>",
-            headers:  headers
+    header.setFilters({
+      "templates": {
+        "settings": {
+          "enable": 1,
+          "template_id": "ce551d09-ce7d-4a48-b5b9-653ad2789702"
         }
+      }
+    });
 
-        smtpTransport.sendMail(mailOptions, function(error, response) {
-            smtpTransport.close();
+    header.setSubstitutions(sub);
 
-            console.log( error || "Message sent");
-        });
-    }
+    // Send usin Nodemailer
+		var headers = { "x-smtpapi": header.jsonString() };
+
+		var smtpTransport = nodemailer.createTransport(GlobalServer.emailSettings);
+
+		var mailOptions = {
+		  from:     "Village.fm <hello@village.fm>",
+		  to:       user.services.facebook.email,
+		  text:     "Hello world",
+		  html:     "<b>Hello world</b>",
+		  headers:  headers
+		}
+
+		smtpTransport.sendMail(mailOptions, function(error, response) {
+		  smtpTransport.close();
+
+		  console.log( error || "Message sent");
+		});
 
     return user;
 });
