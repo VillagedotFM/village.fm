b7a0b0f4fef458240050bf9cb480acf3b440ed27
Joris de Ruiter <joris.ruiter@gmail.com>
2016-08-18 18:51:18 +0200

    Add Custom Logo on Village View via URL in document

--- imports/ui/components/header/header-village/helpers.js
+++ imports/ui/components/header/header-village/helpers.js
@@ -2,5 +2,9 @@ Template.header_village.helpers({
 	village() {
 		const villageSlug = FlowRouter.getParam('villageSlug');
        	return Villages.findOne({slug: villageSlug});
+	},
+	villageLogoUrl(slug) {
+		// TODO: use template string
+		return '/images/img-topbar-' + slug + '.png';
 	}
 });
