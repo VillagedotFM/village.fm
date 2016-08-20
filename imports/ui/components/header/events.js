3d4e1afb8eab39ad244f45c5cab2b9283791b30a
Joris de Ruiter <joris.ruiter@gmail.com>
2016-08-20 12:01:00 +0200

    Signed Up Event

--- imports/ui/components/header/events.js
+++ imports/ui/components/header/events.js
@@ -5,7 +5,19 @@ Template.header.events({
       if (err) {
         throw new Meteor.Error("Facebook login failed");
       } else {
-        console.log("Logged In!");
+        mixpanel.identify(Meteor.userId());
+        console.log(mixpanel.identify(Meteor.userId()));
+        mixpanel.people.set({
+          '$email': Meteor.user().services.facebook.email,
+          '$first_name': Meteor.user().services.facebook.first_name,
+          '$last_name': Meteor.user().services.facebook.last_name,
+          '$name': Meteor.user().services.facebook.name,
+          'gender': Meteor.user().services.facebook.gender
+        });
+        mixpanel.track('Signed Up', {
+          service: 'facebook'
+          // TODO: FB friends properties
+        });
         // $('.sign-up').hide();
       }
     });
