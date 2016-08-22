904e79e2f3ac140013094687f2046f1c7d421c48
Joris de Ruiter <joris.ruiter@gmail.com>
2016-08-22 16:39:44 +0200

    Fixed anonymous user sync

--- imports/ui/components/header/events.js
+++ imports/ui/components/header/events.js
@@ -5,7 +5,24 @@ Template.header.events({
       if (err) {
         throw new Meteor.Error("Facebook login failed");
       } else {
-        mixpanel.identify(Meteor.userId());
+
+        var user = Meteor.user();
+        // FIX: Find another way how to know that a new user is logging in
+        if(user.profile.newUser){
+          mixpanel.alias(Meteor.userId());
+          mixpanel.track('Signed Up', {
+            service: 'facebook'
+            // TODO: FB friends properties
+          });
+          Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.newUser": false}});
+        } else {
+          mixpanel.identify(Meteor.userId());
+          mixpanel.track('Signed In', {
+            service: 'facebook'
+            // TODO: FB friends properties
+          });
+        }
+       
         mixpanel.people.set({
           '$email': Meteor.user().services.facebook.email,
           '$first_name': Meteor.user().services.facebook.first_name,
@@ -13,10 +30,7 @@ Template.header.events({
           '$name': Meteor.user().services.facebook.name,
           'gender': Meteor.user().services.facebook.gender
         });
-        mixpanel.track('Signed Up', {
-          service: 'facebook'
-          // TODO: FB friends properties
-        });
+        
         // $('.sign-up').hide();
       }
     });
