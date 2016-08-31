d53659aa1fc1a05700de7b0eb03e6c44d5ba8727
dylan sanders <sandersd27@gmail.com>
2016-08-30 22:33:10 -0700

    scrolling over iframes now works + scroll to top of feed when time filters change

--- imports/ui/components/sidebar/tabs/events.js
+++ imports/ui/components/sidebar/tabs/events.js
@@ -1,6 +1,7 @@
 Template.tabs.events({
   "click .timeFilter"(event, instance){
     $('.sr-playlist').scrollTop(0);
+    $('.wrapper').scrollTop(0);
     appBodyRef.timeFilter.set($(event.target).data('time'));
 
     mixpanel.track('Clicked Time Filter', {
