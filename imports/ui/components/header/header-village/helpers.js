8db193b2f0614741130e4c9438f8a62d0315f911
dylan sanders <sandersd27@gmail.com>
2016-08-30 21:18:12 -0700

    revert topbar to 60px + fixed mobile topbar styling

--- imports/ui/components/header/header-village/helpers.js
+++ imports/ui/components/header/header-village/helpers.js
@@ -1,11 +1,14 @@
 Template.header_village.helpers({
 	village() {
 		const villageSlug = FlowRouter.getParam('villageSlug');
-		if (villageSlug !== '/')
-			return Villages.findOne({slug: villageSlug});
+		return Villages.findOne({slug: villageSlug});
 	},
 	villageLogoUrl(slug) {
 		// TODO: use template string
-		return 'images/img-topbar-' + slug + '@3x.png';
+		if (slug) {
+			return 'images/img-topbar-' + slug + '@3x.png';
+		}else {
+			return 'images/img-topbar-main@3x.png';
+		}
 	}
 });
