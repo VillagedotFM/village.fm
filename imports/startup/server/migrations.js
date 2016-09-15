a4063f3c86d6e14c91dc0bbdf775512deabf746a
Joris de Ruiter <joris.ruiter@gmail.com>
2016-09-15 10:47:47 +0200

    fix migrations for denormalising

--- imports/startup/server/migrations.js
+++ imports/startup/server/migrations.js
@@ -10,26 +10,80 @@ Meteor.startup(function() {
 });
 
 Migrations.add({
-  version: 1,
-  name: 'Add denormalised data',
+  version: 4,
+  name: 'Add data to villages',
   up: function() {
-    const users = Meteor.users.find();
-    for(let i = 0; i < users.length; i++){
-      let user = users[0];
+    Villages.find().forEach(function(village){
+      const posts = Posts.find({ villages: village._id }).fetch();
+      const profiles = Profiles.find({ createdBy: { $in: village.users } }).fetch();
 
-      Profiles.direct.insert({
-        firstName: user.services.facebook.first_name,
-        lastName: user.services.facebook.last_name,
-        niceName: user.services.facebook.first_name + " " + user.services.facebook.last_name.charAt(0),
-        villagerNum: user.profile.villagerNum,
-        picture: "https://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large",
-        createdBy: user._id
+      Villages.direct.update(village._id, {
+        $set: {
+          posts: posts,
+          profiles: profiles
+        }
+      })
+    });
+  },
+  down: function() {
+    Villages.find().forEach(function(village){
+      Villages.update(village._id, {
+        $unset: {
+          posts: 1,
+          profiles: 1
+        }
+      })
+    });
+  }
+});
+
+
+Migrations.add({
+  version: 3,
+  name: 'Add data to posts',
+  up: function() {
+    Posts.find().forEach(function(post){
+      const comments = Comments.find({ postId: post._id }).fetch();
+      const profile = Profiles.findOne({ createdBy: post.createdBy });
+
+      let upvoteObjects = [];
+      upvoteProfiles = Profiles.find({ createdBy: { $in: post.upvotedBy } });
+
+      post.upvotedBy.forEach(function(upvote, index){
+        upvoteObjects.push({
+          createdBy: upvote,
+          profile: upvoteProfiles[index]
+        })
       });
-    };
 
-    const comments = Comments.find();
-    for(let i = 0; i < comments.length; i++){
-      let comment = comments[i];
+      Posts.direct.update(post._id, {
+        $set: {
+          upvoteObjects: upvoteObjects,
+          comments: comments,
+          profile: profile
+        }
+      });
+    });
+  },
+  down: function() {
+    Posts.find().forEach(function(post){
+      Posts.update(post._id, {
+        $unset: {
+          upvoteObjects: 1,
+          comments: 1,
+          profile: 1
+        }
+      });
+    });
+  }
+});
+
+Migrations.add({
+  version: 2,
+  name: 'Add data to comments',
+  up: function() {
+
+    Comments.find().forEach(function(comment){
       const post = Posts.findOne({ _id: comment.postId });
       const profile = Profiles.findOne({ createdBy: comment.createdBy });
 
@@ -42,52 +96,9 @@ Migrations.add({
           profile: profile
         }
       });
-    };
-
-    const posts = Posts.find();
-    for(let i = 0; i < posts.length; i++){
-      let post = posts[i];
-      const comments = Comments.find({ postId: post._id });
-      const profile = Profiles.findOne({ createdBy: post.createdBy });
-
-      let upvoteObjects = [];
-
-      post.upvotedBy.forEach(function(upvote){
-        upvoteProfile = Profiles.findOne({ createdBy: upvote });
-        upvoteObjects.push({
-          createdBy: upvote,
-          profile: upvoteProfile
-        })
-      });
-
-      Posts.direct.update(post._id, {
-        $set: {
-          upvoteObjects: upvoteObjects,
-          comments: comments,
-          profile: profile
-        }
-      });
-    };
-
-    const villages = Villages.find();
-    for(let i = 0; i < villages.length; i++){
-      let village = villages[0];
-      const posts = Posts.find({ villages: village._id });
-      const profiles = Profiles.find({ createdBy: { $in: village.users } });
-
-      Villages.direct.update(village._id, {
-        $set: {
-          posts: posts,
-          profiles: profiles
-        }
-      })
-    };
-
-
+    });
   },
   down: function() {
-    Profiles.remove({});
-
     Comments.find().forEach(function(comment){
       Comments.update(comment._id, {
         $unset: {
@@ -99,24 +110,26 @@ Migrations.add({
         }
       });
     });
+  }
+});
 
-    Posts.find().forEach(function(post){
-      Posts.update(post._id, {
-        $unset: {
-          upvoteObjects: 1,
-          comments: 1,
-          profile: 1
-        }
+
+Migrations.add({
+  version: 1,
+  name: 'Add profiles',
+  up: function() {
+    Meteor.users.find().forEach(function(user){
+      Profiles.direct.insert({
+        firstName: user.services.facebook.first_name,
+        lastName: user.services.facebook.last_name,
+        niceName: user.services.facebook.first_name + " " + user.services.facebook.last_name.charAt(0),
+        villagerNum: user.profile.villagerNum,
+        picture: "https://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large",
+        createdBy: user._id
       });
     });
-
-    Villages.find().forEach(function(village){
-      Villages.update(village._id, {
-        $unset: {
-          posts: 1,
-          profiles: 1
-        }
-      })
-    });
+  },
+  down: function() {
+    Profiles.remove({});
   }
 });
