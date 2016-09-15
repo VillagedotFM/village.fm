b36f192440e496cc88b7d764546bbb5bb2c3dcaa
dylan sanders <sandersd27@gmail.com>
2016-09-15 00:19:25 -0700

    fix some playback bugs

--- imports/ui/components/sidebar/controls/helpers.js
+++ imports/ui/components/sidebar/controls/helpers.js
@@ -6,13 +6,4 @@ Template.controls.helpers({
       return state === 1 ? 'sr-controls__play--paused' : 'sr-controls__play--play';
     }
   },
-  buffering() {
-    let nowPlaying = appBodyRef.nowPlaying.get();
-    if (nowPlaying && nowPlaying.type === 'youtube') {
-      let state = appBodyRef.state.get();
-      return (state === 1 || state === 2) ? false : true;
-    } else {
-      return false;
-    }
-  }
 });
