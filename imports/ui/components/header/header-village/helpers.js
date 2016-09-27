55fd8cbc56833a14678277420572b222f21814e8
Joris de Ruiter <joris.ruiter@gmail.com>
2016-09-27 09:26:46 +0200

    Fix Header Logo loading in Village

--- imports/ui/components/header/header-village/helpers.js
+++ imports/ui/components/header/header-village/helpers.js
@@ -3,8 +3,6 @@ Template.header_village.helpers({
 		// TODO: use template string
 		if (slug) {
 			return 'images/img-topbar-' + slug + '@3x.png';
-		}else {
-			return 'images/img-topbar-main@3x.png';
 		}
 	}
 });
