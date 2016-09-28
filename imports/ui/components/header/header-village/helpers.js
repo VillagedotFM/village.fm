ecf0f8ddee39ae0156003d94c9cba6c1850a2d89
dylan sanders <sandersd27@gmail.com>
2016-09-27 17:05:33 -0700

    fix topbar assets

--- imports/ui/components/header/header-village/helpers.js
+++ imports/ui/components/header/header-village/helpers.js
@@ -2,7 +2,7 @@ Template.header_village.helpers({
 	villageLogoUrl(slug) {
 		// TODO: use template string
 		if (slug) {
-			return 'images/img-topbar-' + slug + '@3x.png';
+			return '/images/img-topbar-' + slug + '@3x.png';
 		}
 	}
 });
