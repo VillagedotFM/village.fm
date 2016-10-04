68e496363274427ef909b12410613f7bb82b7b0a
dylan sanders <sandersd27@gmail.com>
2016-10-04 03:35:43 -0700

    restrict tune in posting to admins

--- imports/ui/components/content/helpers.js
+++ imports/ui/components/content/helpers.js
@@ -1,5 +1,12 @@
 Template.content.helpers({
   ifVillageSlug() {
     return FlowRouter.getParam('villageSlug');
+  },
+  isTuneIn() {
+    if (Roles.userIsInRole(Meteor.userId(), ['admin'])) {
+      return false;
+    } else {
+      return FlowRouter.getParam('villageSlug') === 'tuneintelaviv';      
+    }
   }
 });
