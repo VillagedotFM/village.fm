05c8f9d1593eeea936d3acc35cd2094b921da61f
Stefan Djokic <stefan.djol3@gmail.com>
2016-10-22 01:44:01 +0200

    Add Request Village Popup

--- imports/ui/components/request-village-popup/request-village-popup.js
+++ imports/ui/components/request-village-popup/request-village-popup.js
@@ -1,3 +1,11 @@
 import './request-village-popup.html';
 import './events.js';
 import './helpers.js';
+
+Template.request_village_popup.onCreated(() => {
+  requestVillageRef = this;
+
+  requestVillageRef.email = new ReactiveVar(null);
+  requestVillageRef.category = new ReactiveVar(null);
+  requestVillageRef.showSuccessPopup = new ReactiveVar(false);
+})
