87bd85a1e9968996e715656433ca89cbf9d2307f
dylan sanders <sandersd27@gmail.com>
2016-11-17 17:34:30 +0100

    stop cycling through topbars

--- imports/ui/components/header/header-village/helpers.js
+++ imports/ui/components/header/header-village/helpers.js
@@ -1,5 +1,13 @@
 Template.header_village.helpers({
 	isMobile: function() {
 		return appBodyRef.mobile.get();
+	},
+	villageTopBar: function() {
+		const villageSlug = FlowRouter.getParam('villageSlug');
+		if (villageSlug) {
+			return villageSlug;
+		} else {
+			return "main";
+		}
 	}
 });
