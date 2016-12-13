f00e81bb0ba537daa3a795ef4ded68b12be8fedf
Stefan Djokic <stefan.djol3@gmail.com>
2016-12-13 13:49:23 +0100

    Refactor header component

--- imports/ui/components/header/events.js
+++ imports/ui/components/header/events.js
@@ -1,40 +1,19 @@
 import { Email } from 'meteor/email';
 
 Template.header.events({
-  "click .header__unloged": function(event, template){
+  'click .vf-header__logo': () => {
+    appBodyRef.showSideMenu.set(!appBodyRef.showSideMenu.get());
+  },
+  "click .vf-header__log-in": function(event, template){
     event.stopPropagation();
     appBodyRef.signUp.set(true);
 
     mixpanel.track('Clicked Sign In Button');
-  },
-  "click .header__logo, click header__logo-mobile"(event, instance) {
-    $('.uploaded-item').hide();
-    $('.sr-playlist__item--inbox').hide();
-    $('.sr-inbox__arrow').removeClass('fa-caret-up');
-  },
-  'click .header__hum': function(event, template){
-    event.stopPropagation();
-    $('.header__hum').toggleClass('header__hum--active');
-    $('.main, .header, .bottom-player, .us-mobile').toggleClass('menu-open');
-  },
-  'submit form': function(event, template) {
-    event.preventDefault();
-
-    var user = Meteor.user();
-    var feedbackText = template.find('#feedback-text');
-
-    Meteor.call('submitFeedbackForm', user.services.facebook.email, user.profile.name, feedbackText.value);
-
-    feedbackText.value = "";
-    $('.feedback__contant').slideToggle();
   },
   'click .tune-in-close': function(event, template) {
     appBodyRef.tuneInBanner.set(false);
   },
   'click .tune-in-link': function(event, template) {
     mixpanel.track('Clicked Buy Tickets Link');
-  },
-  'click .header__logo, click .header__logo-mobile': () => {
-    appBodyRef.showSideMenu.set(!appBodyRef.showSideMenu.get());
   }
 });
