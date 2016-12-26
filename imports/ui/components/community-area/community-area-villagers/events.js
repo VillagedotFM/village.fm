b9925f09027883031e2878b1fffecd891989a5ea
Stefan Djokic <stefan.djol3@gmail.com>
2016-12-26 12:49:57 +0100

    Display top villager dropdown on hover

--- imports/ui/components/community-area/community-area-villagers/events.js
+++ imports/ui/components/community-area/community-area-villagers/events.js
@@ -1,8 +1,4 @@
 Template.community_area_villagers.events({
-  'click .vf-community-area__top-villager': (event) => {
-    $('.vf-community-area__top-villager').removeClass('active');
-    $(event.currentTarget).addClass('active');
-  },
   'click .vf-community-area__top-villagers-header__view-all': (event) => {
     communityAreaVillagersRef.villagersModalActive.set(true);
   },
