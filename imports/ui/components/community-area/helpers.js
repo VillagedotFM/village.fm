35ddeb162f5c94db3fce673b2f85ca3d786a8792
dylan sanders <sandersd27@gmail.com>
2016-12-27 10:31:47 -0500

    add backend village data to community area + only display edit buttons for admins

--- imports/ui/components/community-area/helpers.js
+++ imports/ui/components/community-area/helpers.js
@@ -1,4 +1,23 @@
+import { Villages } from '../../../api/villages/villages.js';
+
 Template.community_area.helpers({
+  'currentVillage': () => {
+    const villageSlug = FlowRouter.getParam('villageSlug');
+    let village;
+
+    if (villageSlug) {    //Handle main village (no slug)
+      village = Villages.findOne({'friendlySlugs.slug.base': villageSlug});
+    } else {
+      village = Villages.findOne({'friendlySlugs.slug.base': "main"});
+    }
+
+    if (village) {
+      return village;
+    }
+  },
+  'isAdmin': function() {
+    return _.contains(this.admins, Meteor.userId());
+  },
   'inviteButtonActive': () => {
     return communityAreaRef.inviteButtonActive.get();
   },
